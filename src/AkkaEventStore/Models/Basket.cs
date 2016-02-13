f2115c5861572a4425893d5a262678f0539e096c
Vlad Kosarev <vlad@webmustflow.com>
2016-02-13 16:13:29 -0500

    Adding missing files

---
+++ src/AkkaEventStore/Models/Basket.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+
+namespace AkkaEventStore.Models
+{
+    public class Basket
+    {
+        public Basket()
+        {
+            LineItems = new List<LineItem>();
+        }
+
+        public string Id { get; set; }
+        public List<LineItem> LineItems { get; set; }
+        public Address Address { get; set; }
+    }
+}
